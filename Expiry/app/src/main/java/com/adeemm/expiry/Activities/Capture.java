e5f7b35da3b48e59cd7b9cfb5b8f5e4a0cd171d4
JasonBLyle <72158915+JasonBLyle@users.noreply.github.com>
2021-04-18 19:16:09 -0400

    Add files via upload

--- Expiry/app/src/main/java/com/adeemm/expiry/Activities/Capture.java
+++ Expiry/app/src/main/java/com/adeemm/expiry/Activities/Capture.java
@@ -3,6 +3,10 @@ package com.adeemm.expiry.Activities;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+ * This is the activity that scans the barcode.
+ * This is implements the zxing open source barcode library.
+ */
 
 public class Capture extends CaptureActivity {
 
